17512c0ddc03d8298d6fc47397a3b036b49d17c6
Wangley Vieira <wangleymsv10@hotmail.com>
2021-02-18 22:11:26 -0300

    rotas de ControllerClientes e ControllerClientes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControllerVeiculos;
+use App\Http\Controllers\ControllerClientes;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/veiculo', [ControllerVeiculos::class, 'index']);
+Route::get('/cliente', [ControllerClientes::class, 'index']);
