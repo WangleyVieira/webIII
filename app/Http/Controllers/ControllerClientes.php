09f2c57bca4f78d3be87c5edb395d64c85339a0f
Wangley Vieira <wangleymsv10@hotmail.com>
2021-03-02 21:02:58 -0300

    adicionado funcao novo e salvar

--- app/Http/Controllers/ControllerClientes.php
+++ app/Http/Controllers/ControllerClientes.php
@@ -17,7 +17,25 @@ class ControllerClientes extends Controller
 
     function novo(){
         $cliente = new Clientes();
+        $cliente->id = 0;
+        $cliente->nome ="";
+        $cliente->cpf="";
+        $cliente->email="";
+        $cliente->cep="";
+        $cliente->telefone="";
+        $cliente->id_veiculo_cliente=0;
+
+        $cliente = Clientes::all();
         
         return view("clientes.frmCliente", compact('cliente'));
+    }
+
+    function salvar(Request $request){
+        if($request->input("id")==0){
+            $cliente = new Clientes();
+        } else{
+            $cliente->save();
+            return redirect()->route("veiculo_novo");
+        }
     }
 }
